f02504b909082f1dcbac9c98b9791be1c4453d67
Chakravarthi Darimireddi <darimireddy@googlemail.com>
2013-01-10 22:19:21 +0530

    Inserted delay into the transaction.

--- ConcurrencyWAR/src/main/java/com/mycompany/worker/WeatherWorker.java
+++ ConcurrencyWAR/src/main/java/com/mycompany/worker/WeatherWorker.java
@@ -5,6 +5,8 @@
 package com.mycompany.worker;
 
 import java.util.concurrent.Future;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.ejb.AsyncResult;
 import javax.ejb.Asynchronous;
 import javax.ejb.Stateless;
@@ -32,12 +34,20 @@ public class WeatherWorker {
     BindingProvider bindingProvider = (BindingProvider) globalWeatherSoap;
     bindingProvider.getRequestContext().put(
             BindingProvider.ENDPOINT_ADDRESS_PROPERTY, "http://localhost:6789/globalweather.asmx");
-
+    insertDealy(1500);
     return globalWeatherSoap.getWeather(city, country);
   }
 
   @Asynchronous
   public Future<String> getWeatherAsync(String country, String city) {
     return new AsyncResult<String>(getWeather(country, city));
+  }
+
+  private void insertDealy(long milliSeconds) {
+    try {
+      Thread.sleep(milliSeconds);
+    } catch (InterruptedException ex) {
+      Logger.getLogger(WorldWorker.class.getName()).log(Level.SEVERE, null, ex);
+    }
   }
 }
