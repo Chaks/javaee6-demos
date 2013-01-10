615a694ef70c447d30342f95f50d142f63527bed
Chakravarthi Darimireddi <darimireddy@googlemail.com>
2013-01-10 22:19:05 +0530

    Inserted delay into the transaction.

--- ConcurrencyWAR/src/main/java/com/mycompany/worker/WorldWorker.java
+++ ConcurrencyWAR/src/main/java/com/mycompany/worker/WorldWorker.java
@@ -16,6 +16,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.annotation.Resource;
 import javax.ejb.AsyncResult;
+import javax.ejb.Asynchronous;
 import javax.ejb.Stateless;
 import javax.ejb.TransactionManagement;
 import javax.ejb.TransactionManagementType;
@@ -30,45 +31,48 @@ import javax.transaction.UserTransaction;
 @Stateless
 @TransactionManagement(TransactionManagementType.BEAN)
 public class WorldWorker {
-  
+
   @PersistenceContext(unitName = "com.mycompany.demo_ConcurrencyWAR_war_1.0-SNAPSHOTPU")
   EntityManager entityManager;
   @Resource
   UserTransaction userTransaction;
-  
+
   public List<City> getCities() {
     CityJpaController cityJpaController =
             new CityJpaController(userTransaction, entityManager.getEntityManagerFactory());
-    insertDealy(1000);
+    insertDealy(1500);
     return cityJpaController.findCityEntities();
   }
-  
+
   public List<Country> getCountries() {
     CountryJpaController countryJpaController =
             new CountryJpaController(userTransaction, entityManager.getEntityManagerFactory());
-    insertDealy(500);
+    insertDealy(1500);
     return countryJpaController.findCountryEntities();
   }
-  
+
   public List<CountryLanguage> getLanguages() {
     CountryLanguageJpaController countryLanguageJpaController =
             new CountryLanguageJpaController(userTransaction, entityManager.getEntityManagerFactory());
-    insertDealy(500);
+    insertDealy(1500);
     return countryLanguageJpaController.findCountryLanguageEntities();
   }
-  
+
+  @Asynchronous
   public Future<List<City>> getCitiesAsync() {
     return new AsyncResult<List<City>>(getCities());
   }
-  
+
+  @Asynchronous
   public Future<List<Country>> getCountriesAsync() {
     return new AsyncResult<List<Country>>(getCountries());
   }
-  
+
+  @Asynchronous
   public Future<List<CountryLanguage>> getLanguagesAsync() {
     return new AsyncResult<List<CountryLanguage>>(getLanguages());
   }
-  
+
   private void insertDealy(long milliSeconds) {
     try {
       Thread.sleep(milliSeconds);
